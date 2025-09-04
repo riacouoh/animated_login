78c6c05a8aa2b8cf535080a2160b9b3df582350e
Vale Naranjo <riacouoh@gmail.com>
2025-09-04 10:25:10 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,15 +9,73 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+    bool _obscureText = false;
+
+    @override
+    void initState(){
+      super.initState();
+      _obscureText=true;
+    }    
+
   @override
   Widget build(BuildContext context) {
+
+    //para obtener el tamaño de la pantalla: :D
+
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       //evia nudge o cámaras frontales
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: RiveAnimation.asset("assets/animated_login_character.riv"))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset("assets/animated_login_character.riv")
+                ),
+              
+              // espacio entre oso y texto
+              SizedBox(height: 10,),
+          
+              // campo de texto
+              TextField(
+                keyboardType: TextInputType.emailAddress, // <-- para que aparezca @ en móviles
+                decoration: InputDecoration(
+                  hintText: "E-Mail",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )
+                ),
+              ),
+          
+              const SizedBox(height: 10,),
+          
+              // campo de texto
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(Icons.key),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off,),
+                      onPressed: () {
+                      setState(() {
+                         _obscureText = !_obscureText;
+                       });
+                    }, )
+                ),
+              ),            
+          
+          
+            ],
+          ),
         )),
     );
   }
